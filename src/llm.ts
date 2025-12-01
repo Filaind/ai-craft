e0f85aaf6a000f7de647a7416f23268e457559bf
Findz <stanislav.shemyagin@gmail.com>
2025-12-01 16:46:37 +0200

    Refactor chat completion logic in llm.ts to support dynamic message handling and update model to qwen/qwen3-coder-30b. Add type import for ChatCompletionMessageParam and enhance tool call processing. Ensure proper logging of tool calls and responses.

--- src/llm.ts
+++ src/llm.ts
@@ -1,5 +1,6 @@
 import OpenAI from "openai";
 import { LLMFunctions } from "./functions/llm-functions";
+import type { ChatCompletionMessageParam } from "openai/resources";
 
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY || "",
@@ -8,22 +9,44 @@ const client = new OpenAI({
 
 await LLMFunctions.registerAllFunctions();
 
-const response = await client.chat.completions.create({
-    model: "openai/gpt-oss-20b",
-    tool_choice: "auto",
-    tools: LLMFunctions.getFunctionList() as any,
-    messages: [{
-        role: "user",
-        content: "Get current time"
-    }]
-});
-
-const choice = response.choices[0]!
-choice.message.tool_calls?.forEach((tool_call) => {
-    if (tool_call.type == "function") {
-        const function_name = tool_call.function.name
-        const function_arguments = JSON.parse(tool_call.function.arguments)
-        const function_result = LLMFunctions.invokeFunction(function_name, function_arguments)
-        console.log(function_result)
-    }
+let chat: Array<ChatCompletionMessageParam> = [];
+chat.push({
+    role: "user",
+    content: "Get current time and weather"
 })
+
+async function getResponse() {
+    const response = await client.chat.completions.create({
+        model: "qwen/qwen3-coder-30b",
+        tool_choice: "auto",
+        tools: LLMFunctions.getFunctionList() as any,
+        messages: chat
+    });
+
+    const choice = response.choices[0]!
+    if (choice.message.tool_calls && choice.message.tool_calls.length > 0) {
+        console.log("Tool calls", choice.message.tool_calls.length)
+        choice.message.tool_calls?.forEach((tool_call) => {
+            if (tool_call.type == "function") {
+                console.log(tool_call)
+                const function_name = tool_call.function.name
+                const function_arguments = JSON.parse(tool_call.function.arguments)
+                const function_result = LLMFunctions.invokeFunction(function_name, function_arguments)
+                console.log(function_result)
+                chat.push({
+                    role: "tool",
+                    tool_call_id: tool_call.id,
+                    content: JSON.stringify(function_result)
+                })
+            }
+        })
+        return getResponse()
+    }
+    else{
+        console.log(choice.message.content)
+        return choice.message.content
+    }
+
+}
+
+getResponse()
