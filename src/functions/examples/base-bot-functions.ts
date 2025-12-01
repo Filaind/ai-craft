5e080664d62353b0b24cffe0b9febad6245e233f
Findz <stanislav.shemyagin@gmail.com>
2025-12-01 21:41:25 +0200

    Refactor entity retrieval in LLM function by introducing getNearbyEntities method. Update function registration to return all nearby entities within a specified distance, enhancing bot's interaction capabilities.

--- src/functions/examples/base-bot-functions.ts
+++ src/functions/examples/base-bot-functions.ts
@@ -2,26 +2,32 @@ import type { Bot } from "../../bot"
 import type { Entity } from "prismarine-entity"
 import { LLMFunctions } from "../llm-functions"
 
+export function getNearbyEntities(bot: Bot, maxDistance = 16) {
+    let entities: { entity: Entity, distance: number }[] = [];
+    for (const entity of Object.values(bot.mineflayerBot!.entities)) {
+        const distance = entity.position.distanceTo(bot.mineflayerBot!.entity.position);
+        if (distance > maxDistance) continue;
+        entities.push({ entity: entity, distance: distance });
+    }
+    entities.sort((a, b) => a.distance - b.distance);
+    let res = [];
+    for (let i = 0; i < entities.length; i++) {
+        res.push(entities[i]?.entity);
+    }
+    return res;
+}
+
 LLMFunctions.register({
-    name: "find nearest entity",
-    description: "Find the nearest entity to the bot",
+    name: "get nearby entities",
+    description: "Get all entities nearby the bot",
     parameters: {
         type: "object",
         properties: {
-            entityType: { type: "string", enum: ['mob', 'player'] },
-            name: { type: "string", description: "cow, sheep, etc." }
+            maxDistance: { type: "number", description: "The maximum distance to the entity", default: 16 }
         }
     },
-    function: (args: { bot: Bot, entityType: 'mob' | 'player', name: string }) => {
-        let filter = (e: Entity) => e.type === args.entityType
-        if (args.name) {
-            filter = (e: Entity) => e.type === args.entityType && e.name === args.name
-        }
-        const entity = args.bot.mineflayerBot!.nearestEntity(filter)
-        console.log(entity)
-        return {
-            entity: entity
-        }
+    function: (args: { bot: Bot, maxDistance: number }) => {
+        return getNearbyEntities(args.bot, args.maxDistance)
     },
     strict: true,
     type: 'function'
